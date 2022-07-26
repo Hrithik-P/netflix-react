a6aafa2e5eb90c13cf910dd5bacaed4ab6a4451f
Hrithik-P <hrithik00g@gmail.com>
2022-07-26 16:10:01 +0530

    first commit

---
+++ netflix/src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css"
+function NavBar() {
+  return (
+    <div className="navbar">
+      <img className="logo" src="/header/netflix-logo.png" alt="" srcset="" />
+      <img className="avatar" src="/header/avatar.jpg" alt="" srcset="" />
+    </div>
+  );
+}
+
+export default NavBar;
